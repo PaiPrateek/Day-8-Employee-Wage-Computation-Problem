2304298de9ef2add1d489d11b6a67ea4e33cca93
Prateek Pai <paiprateek123@gmail.com>
2022-06-15 23:06:27 +0530

    Calculating Employee Wage for a Month

--- EmployeeWageComputationProblem/EmpWage.cs
+++ EmployeeWageComputationProblem/EmpWage.cs
@@ -14,27 +14,45 @@ namespace EmployeeWageComputationProblem
             int Absent = 0;
             int PartTime = 2;
             int EmpWagePerHour = 20;
+            int WorkingDaysPerMonth = 20;
             int EmpHrs = 0;
             int EmpWage = 0;
-            Random random = new Random();
-            int Attendence = random.Next(0, 3);
-            switch (Attendence)
+            int TotaklEmpWage = 0;
+            int TotalWorkingDays = 0;
+            int FullDay=0, AbsentDay=0, PartDay=0;
+            for (int day = 0; day < WorkingDaysPerMonth; day++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Absent");
-                    EmpHrs = 0;
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Present (Full Time)");
-                    EmpHrs = 8;
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Present (Part Time) ");
-                    EmpHrs = 4;
-                    break;
+                Random random = new Random();
+                int Attendence = random.Next(0, 3);
+                switch (Attendence)
+                {
+                    case 0:
+                        Console.WriteLine("Employee is Absent");
+                        EmpHrs = 0;
+                        AbsentDay++;
+                        break;
+                    case 1:
+                        Console.WriteLine("Employee is Present (Full Time)");
+                        EmpHrs = 8;
+                        FullDay++;
+                        break;
+                    case 2:
+                        Console.WriteLine("Employee is Present (Part Time) ");
+                        EmpHrs = 4;
+                        PartDay++;
+                        break;
+                }
+                Console.WriteLine("...............................");
+                EmpWage = EmpHrs * EmpWagePerHour;
+                TotaklEmpWage += EmpWage;
+                Console.WriteLine("Employee Daily Wage is : " + EmpWage);
+
             }
-            EmpWage = EmpHrs * EmpWagePerHour;
-            Console.WriteLine("Employee Daily Wage is : " +EmpWage);
+            Console.WriteLine("Full Day Presence is :"+FullDay);
+            Console.WriteLine("Part Time Presnece is : " + PartDay);
+            Console.WriteLine("Absent is :" + AbsentDay);
+            Console.WriteLine("Total Employee Wage is :" + TotaklEmpWage);  
+
         }
     }
 }
